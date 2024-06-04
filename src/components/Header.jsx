e279aedc61cd1f6202abf3460411e24a94c825e8
ErwinAdriel <adrielquispe87@gmail.com>
2024-06-04 00:55:31 -0300

    feat: buscador en header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ export default function Header(){
 
     return(
         <header class="relative">
-            <div class="bg-slate-950 text-white w-full h-10 px-20">
+            <div class="bg-slate-900 text-white w-full h-10 px-20">
                 <div class="mx-auto h-full">
                     <div class="flex justify-between items-center h-full">
                         <div class="topbar-nav sm:block hidden">
@@ -37,16 +37,16 @@ export default function Header(){
                     <div class="relative h-full">
                         <div class="flex justify-between items-center h-full">
                             <div class="text-3xl font-bold"><span>AvComputing</span></div>
-                            {/*<div class="w-[517px] h-[44px]">
-                                <div class="w-full h-full flex items-center border border-qgray-border bg-white">
-                                    <div class="flex-1 bg-red-500 h-full">
-                                        <form action="#" class="h-full">
-                                            <input type="text" class="search-input" placeholder="Buscar producto..."/>
-                                        </form>
-                                    </div>
-                                    <button class="w-[93px] h-full text-sm font-600 bg-qh3-blue text-white" type="button">Buscar</button>
+                            <div class="flex w-[517px] h-[44px] border border-slate-400">
+                                <div class="w-full h-full flex items-center bg-white">
+                                    <form action="#" class="h-full w-full">
+                                        <input type="text" class="search-input h-full w-full px-4 focus:outline-none" placeholder="Buscar producto..."/>
+                                    </form>
                                 </div>
-    </div>*/}
+                                <div class="border-2 border-black px-3 flex items-center bg-slate-900 text-white">
+                                    <button>Buscar</button>
+                                </div>
+                            </div>
                             <div class="flex space-x-6 items-center text-2xl">
                                 <div class="relative"><a href="#"><span><MdFavoriteBorder /></span></a></div>
                                 <div class="relative"><a href="#"><span><IoCartOutline /></span></a></div>
@@ -56,7 +56,7 @@ export default function Header(){
                     </div>
                 </div>
             </div>
-            <div class="bg-slate-950 w-full h-[60px] relative z-30 lg:block hidden text-white px-20">
+            <div class="bg-slate-900 w-full h-[60px] relative z-30 lg:block hidden text-white px-20">
                 <div class="w-full h-full relative">
                     <div class="w-full h-full flex justify-between items-center">
                         <div class="flex xl:space-x-7 space-x-3 items-center">
@@ -72,7 +72,7 @@ export default function Header(){
                                         <ul>
                                             {
                                                 SubMenus.map((submenu)=>(
-                                                    <li key={submenu}><a href="#"><div class="flex items-center px-5 h-10  bg-white transition-all duration-300 ease-in-out cursor-pointer text-black hover:bg-slate-950 hover:text-white">{submenu}</div></a></li>
+                                                    <li key={submenu}><a href="#"><div class="flex items-center px-5 h-10  bg-white transition-all duration-300 ease-in-out cursor-pointer text-black hover:bg-slate-900 hover:text-white">{submenu}</div></a></li>
                                                 ))
                                             }
                                             
@@ -85,7 +85,7 @@ export default function Header(){
                                 <ul class="flex xl:space-x-10 space-x-5">
                                     {
                                         Menus.map((menu)=>(
-                                            <li key={menu}><div class="px-2 py-[18px] transition-all duration-300 ease-in-out cursor-pointer hover:bg-white hover:text-black">{menu}</div></li>        
+                                            <li key={menu}><div class="px-2 py-[18px]  cursor-pointer">{menu}</div></li>        
                                         ))
                                     }
                                 </ul>
